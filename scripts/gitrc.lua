618898d15fe6b70bac3c0ba7ab63445c9021e21e
generalwrex <bjdroot@gmail.com>
2016-05-25 00:44:46 -0700

    Localized DEBUG and set to false

--- scripts/gitrc.lua
+++ scripts/gitrc.lua
@@ -2,14 +2,14 @@
 Copyright (c) 2016 generalwrex, bubbus
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
+of this software and associated documentation files (the "Script"), to deal
+in the Script without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+The above copyright notice shall be included in all
+copies or substantial portions of the Script.
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -71,7 +71,7 @@ Github Repository Checker v1
 	
 */
 
-DEBUG = true
+local DEBUG = false
 
 GitRC = GitRC or {}
 
